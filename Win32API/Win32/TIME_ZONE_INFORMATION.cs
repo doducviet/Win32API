90411b3d0e60c78e1e52c7e1c55ebca2f627acb2
iFeng Huang <373209457@qq.com>
2016-02-29 00:28:39 +0800

    Init commit

---
+++ Win32API/Win32/TIME_ZONE_INFORMATION.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Win32
+{
+	public struct TIME_ZONE_INFORMATION
+	{
+		public int Bias;
+
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+		public string StandardName;
+
+		public SYSTEMTIME StandardDate;
+
+		public int StandardBias;
+
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+		public string DaylightName;
+
+		public SYSTEMTIME DaylightDate;
+
+		public int DaylightBias;
+	}
+}
